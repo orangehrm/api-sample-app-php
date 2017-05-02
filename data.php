39f7ffd603decdbcff3a87bf39daca970e61b166
lakshithaorange <lakshitha@orangehrmlive.com>
2017-05-02 12:41:32 +0530

    API:SampleAPP:data.php , getting data php

--- data.php
+++ data.php
@@ -17,68 +17,31 @@
  * Boston, MA  02110-1301, USA
  */
 
-require_once 'vendor/autoload.php';
+
+
+require_once 'config.php';
+
+require_once 'lib/util.php';
+
 
 use Orangehrm\API\Client;
 use Orangehrm\API\HTTPRequest;
 
 
-
-$client = new Client('http://localhost/','123','hrm');
-
-$date = date("Y-m-d");
-
-$latDay = date('Y-m-d',strtotime("30 days"));
-
-$tomorrow = date('Y-m-d',strtotime("+1 days"));
-
-$paramString = 'employee/event?fromDate='.$latDay.'&toDate='.$tomorrow;
-
-$str2 ='employee/event?fromDate=2017-04-01&toDate=2017-04-30&type=employee&event=SAVE';
-
-$request = new HTTPRequest($str2);
+$client = new Client($config->host, $config->clientId, $config->clientSecret);
+$request = new HTTPRequest(getEventEndPoint());
 $result = $client->get($request)->getResult();
 
-$leaveData = null;
+//Load the event Data
 
-try {
+$event =$_POST["event"];
+$type = $_POST["type"];
+$employeeId = $_POST["id"];
 
-  $db = new DataBase();
-
-  //  $createDB = new CreateTable();
-  // $createDB->createTable($db);
-//  $insert = new InsertData();
-//  $insert->insertTable($db,$result);
-//  $eventData ;
-
-  // get Leave data
-
-  $leaveRequestsUrl = 'leave/search?reject=false&cancelled=false&pendingApproval=true&scheduled=false&taken=false&pastEmployee&page=0&limit=10';
-
-
-  $leaveRequest = new HTTPRequest($leaveRequestsUrl);
-
-  $leaveResults = $client->get($leaveRequest)->getResult();
-
-  $leavesUrl      = 'leave/search?reject=false&cancelled=false&pendingApproval=false&scheduled=true&taken=true&pastEmployee&page=0&limit=10&fromDate='.$date.'&toDate='.$date;
-
-  $leavesToday = new HTTPRequest($leavesUrl);
-
-  $leavesInToday = $client->get($leavesToday)->getResult();
-
-
-  $data = new RetrieveData();
-  $eventData =  $data->getData($db);
-
-  $events['data'] = $eventData;
-  $events['leaveRequests'] = $leaveResults;
-  $events['onLeave'] = $leavesInToday;
-  $events['newMembers']   = $result;
-
-  echo json_encode($events);
-
-} catch (Exception $e) {
-  printf($e->getMessage());
+if($event == 'getEventData'){
+    getEventData($type,$employeeId,$client);
+}else if ($event == 'createEvents'){
+    createEvents($client);
 }
 
 ?>
